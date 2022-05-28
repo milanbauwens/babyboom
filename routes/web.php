8daefb927a75f03963d40ff02954a16f572a6a79
milanbauwens <milanbauwens70@gmail.com>
2022-05-28 17:13:07 +0200

    👀 Fixed bugs

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function(){
+    return redirect()->route('landing');
+});
+
+Route::get('/welcome', function () {
     return view('pages.landingpage');
 })->middleware(['guest'])->name('landing');
 
