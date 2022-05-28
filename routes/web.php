6582a33447c659c703995a8510a8237ae686b71b
milanbauwens <milanbauwens70@gmail.com>
2022-05-28 16:40:41 +0200

    👀 Fixed bugs

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.landingpage');
-})->name('landing');
+})->middleware(['guest'])->name('landing');
 
 Route::get('/dashboard', [DashboardController::class, 'show'])->middleware(['auth'])->name('dashboard');
 
